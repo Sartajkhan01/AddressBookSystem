1ab234aa3a41cb5d6d1bf6626756189a0a1a3b6b
Sartajkhan01 <106169841+Sartajkhan01@users.noreply.github.com>
2022-06-14 23:37:41 +0530

    Add files via upload

---
+++ AddressBookSystem/AddMultiplesContact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddMultiplesContact
+    {
+
+        List<Contact> addressList = new List<Contact>();
+        public void AddContact(Contact contact) // This Method Will Add the Details of Customer
+        {
+            addressList.Add(contact);
+        }
+        public void Display() // This Method wii Display the Details
+        {
+            foreach (var contact in addressList)
+            {
+                Console.WriteLine(contact.FirstName + " " + contact.LastName);
+                Console.WriteLine("Last Name: " + contact.LastName);
+                Console.WriteLine("Address : " + contact.Address);
+                Console.WriteLine("City : " + contact.City);
+                Console.WriteLine("State : " + contact.State);
+                Console.WriteLine("Zip : " + contact.Zip);
+                Console.WriteLine("PhoneNumber : " + contact.PhoneNumber);
+                Console.WriteLine("Email : " + contact.Email);
+            }
+        }
+}
